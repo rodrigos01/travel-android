bd0ce602a15a3afc5556ee99e2e193f6b5066a6f
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2018-09-29 13:15:15 -0300

    Removed Litho libraries

--- app/src/main/java/com/combah/travel2/TravelApp.kt
+++ app/src/main/java/com/combah/travel2/TravelApp.kt
@@ -1,23 +1,15 @@
 package com.combah.travel2
 
 import com.combah.travel2.di.DaggerAppComponent
-import com.facebook.drawee.backends.pipeline.Fresco
-import com.facebook.soloader.SoLoader
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class TravelApp : DaggerApplication() {
-    override fun onCreate() {
-        super.onCreate()
-
-        SoLoader.init(this, false)
-        Fresco.initialize(this)
-    }
 
     override fun applicationInjector(): AndroidInjector<DaggerApplication> {
         val component = DaggerAppComponent.builder()
-            .application(this)
-            .build()
+                .application(this)
+                .build()
         component.inject(this)
 
         return component
