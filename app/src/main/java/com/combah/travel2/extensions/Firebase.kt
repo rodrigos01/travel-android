5367cf95f5c3baec9b3c8034b5d4ca58b4f1fc6b
Rodrigo Santos <rodrigo.santos@iwsbrazil.com>
2018-11-28 13:27:57 -0200

    trip repository refactor

--- app/src/main/java/com/combah/travel2/extensions/Firebase.kt
+++ app/src/main/java/com/combah/travel2/extensions/Firebase.kt
@@ -3,6 +3,7 @@ package com.combah.travel2.extensions
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.DocumentSnapshot
+import com.google.firebase.firestore.ListenerRegistration
 import io.reactivex.Observable
 
 inline fun <reified T : Any> CollectionReference.asObservable(noinline converter: ((DocumentSnapshot) -> T)? = null): Observable<List<T>> {
@@ -19,8 +20,9 @@ inline fun <reified T : Any> CollectionReference.asObservable(noinline converter
 }
 
 inline fun <reified T : Any> DocumentReference.asObservable(noinline converter: ((DocumentSnapshot) -> T)? = null): Observable<T> {
-    return Observable.create { emitter ->
-        addSnapshotListener { snapshot, exception ->
+    lateinit var registration: ListenerRegistration
+    return Observable.create<T> { emitter ->
+        registration = addSnapshotListener { snapshot, exception ->
             val value = snapshot?.let { snapshotToObject(it, converter) }
             when {
                 value != null -> emitter.onNext(value)
@@ -28,6 +30,8 @@ inline fun <reified T : Any> DocumentReference.asObservable(noinline converter: 
                 else -> emitter.onError(Throwable())
             }
         }
+    }.doOnDispose {
+        registration.remove()
     }
 }
 
