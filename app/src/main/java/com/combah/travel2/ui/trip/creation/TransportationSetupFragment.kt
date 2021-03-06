5ede0dff93562830927928ce7267dd2835fbbb53
Rodrigo Santos <rodrigo.santos01@gmail.com>
2021-03-06 00:34:25 -0500

    Refactor of TransportationSetupFragment to Jetpack View binding.

--- app/src/main/java/com/combah/travel2/ui/trip/creation/TransportationSetupFragment.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/creation/TransportationSetupFragment.kt
@@ -6,9 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
 import androidx.transition.AutoTransition
 import com.combah.travel2.databinding.FragmentTransportationSetupBinding
+import com.combah.travel2.ui.widget.DatePickerEditText
+import com.combah.travel2.ui.widget.makeDatePickerDialog
+import java.util.*
 
 
 class TransportationSetupFragment : Fragment() {
@@ -19,20 +24,44 @@ class TransportationSetupFragment : Fragment() {
         sharedElementReturnTransition = AutoTransition()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
 
-        val viewModel = ViewModelProviders.of(this)
-                .get(TransportationSetupViewModel::class.java)
+        val viewModel = ViewModelProvider(this)
+            .get(TransportationSetupViewModel::class.java)
 
         val binding = FragmentTransportationSetupBinding.inflate(inflater, container, false)
-        binding.lifecycleOwner = this
-        binding.viewModel = viewModel
+
+        binding.pickerDepartureDate.setUpDate(viewModel.departureDate)
+        binding.pickerReturnDate.setUpDate(
+            viewModel.returnDate,
+            minDateLiveData = viewModel.departureDate
+        )
 
         (activity as? AppCompatActivity)?.setSupportActionBar(binding.toolbar)
         (activity as? AppCompatActivity)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         return binding.root
+    }
+
+    private fun DatePickerEditText.setUpDate(
+        dateLiveData: MutableLiveData<Date>,
+        minDateLiveData: LiveData<Date>? = null
+    ) {
+        dateLiveData.observe(viewLifecycleOwner) {
+            date = it
+        }
+        minDateLiveData?.observe(viewLifecycleOwner) {
+            minDate = it
+        }
+        setOnClickListener {
+            makeDatePickerDialog(context, date, minDate = minDate) { newDate ->
+                date = newDate
+                dateLiveData.value = newDate
+            }
+        }
     }
 
 
