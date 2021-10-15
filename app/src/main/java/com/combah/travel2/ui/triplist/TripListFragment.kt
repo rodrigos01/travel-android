3fb446060f1c19f8489663daedac43f903c2c749
Rodrigo Santos <rodrigo.santos01@gmail.com>
2021-10-15 00:12:01 -0400

    converting trip list to jetpack compose

--- app/src/main/java/com/combah/travel2/ui/triplist/TripListFragment.kt
+++ app/src/main/java/com/combah/travel2/ui/triplist/TripListFragment.kt
@@ -5,13 +5,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import coil.load
 import com.combah.travel2.databinding.FragmentTripListBinding
-import com.combah.travel2.databinding.TripListItemBinding
-import com.combah.travel2.model.data.Trip
-import com.combah.travel2.ui.widget.createAdapter
+import com.combah.travel2.ui.triplist.composable.TripListItem
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class TripListFragment : Fragment() {
@@ -25,19 +26,17 @@ class TripListFragment : Fragment() {
 
         val binding = FragmentTripListBinding.inflate(inflater, container, false)
 
-        val adapter = createAdapter<Trip, TripListItemBinding> { itemBinding, trip ->
-            itemBinding.title.text = trip.name
-            itemBinding.cover.load(trip.coverImage)
-        }
-        adapter.onItemClicked.observe(viewLifecycleOwner) {
-            val action = TripListFragmentDirections.actionTripListFragmentToTripFragment(it.id)
-            findNavController().navigate(action)
-        }
-
-        binding.tripList.adapter = adapter
-
-        viewModel.trips.observe(viewLifecycleOwner) {
-            adapter.setItems(it)
+        binding.composeContainer.setContent {
+            val trips by viewModel.trips.observeAsState(emptyList())
+            LazyColumn {
+                items(trips) { trip ->
+                    TripListItem(name = trip.name, coverImageUrl = trip.coverImage, onClick = {
+                        val action =
+                            TripListFragmentDirections.actionTripListFragmentToTripFragment(trip.id)
+                        findNavController().navigate(action)
+                    })
+                }
+            }
         }
 
         return binding.root
