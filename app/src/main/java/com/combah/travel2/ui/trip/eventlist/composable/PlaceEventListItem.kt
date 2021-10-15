e6dcbcabb819dcc38e4f4849c350d6e03c3d86cf
Rodrigo Santos <rodrigo.santos01@gmail.com>
2021-10-14 23:47:18 -0400

    updating deps and replacing glide with coil

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/PlaceEventListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/PlaceEventListItem.kt
@@ -1,5 +1,6 @@
 package com.combah.travel2.ui.trip.eventlist.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.aspectRatio
@@ -15,19 +16,19 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.combah.travel2.model.data.Place
 import com.combah.travel2.ui.data.PlaceEvent
-import dev.chrisbanes.accompanist.glide.GlideImage
 import java.util.*
 
 @Composable
 fun PlaceEventListItem(event: PlaceEvent) {
     BoxWithConstraints {
-        GlideImage(
+        Image(
+            painter = rememberImagePainter(data = event.currentPlace.coverImage ?: ""),
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(2.33f),
-            data = event.currentPlace.coverImage ?: "",
             contentDescription = "Place Description",
             contentScale = ContentScale.FillWidth
         )
