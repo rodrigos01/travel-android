efa34927b901256382468512f6ea470ebc6656ba
Rodrigo Santos <rodrigo.santos01@gmail.com>
2022-12-09 11:32:04 -0500

    Fixing date picker

--- app/src/main/java/com/combah/travel2/extensions/Date.kt
+++ app/src/main/java/com/combah/travel2/extensions/Date.kt
@@ -6,10 +6,22 @@ import java.util.*
 
 fun Date.asCalendar(): Calendar = Calendar.getInstance().apply { time = this@asCalendar }
 
-fun dateFromString(value: String, format: String? = "yyyy-MM-dd'T'HH:mm"): Date {
+fun dateFromString(value: String, format: String? = "yyyy-MM-dd'T'HH:mm"): Date? {
     return SimpleDateFormat(format, Locale.getDefault()).parse(value)
 }
 
 fun Date.format(style: Int = DateFormat.MEDIUM): String {
     return SimpleDateFormat.getDateInstance(style).format(this)
+}
+
+fun dateFrom(timestamp: Long, originTimezone: TimeZone, targetTimeZone: TimeZone = TimeZone.getDefault()): Date {
+    val originCalendar = Calendar.getInstance(originTimezone)
+    originCalendar.timeInMillis = timestamp
+    val targetCalendar = Calendar.getInstance(targetTimeZone)
+    targetCalendar.set(
+        originCalendar.get(Calendar.YEAR),
+        originCalendar.get(Calendar.MONTH),
+        originCalendar.get(Calendar.DAY_OF_MONTH),
+    )
+    return targetCalendar.time
 }
