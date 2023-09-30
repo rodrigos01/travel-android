ffad7633d3dd6008f51846c25c4f4e219db200e0
Rodrigo Santos <rodrigo.santos01@gmail.com>
2023-09-30 19:10:34 -0400

    Migrating DatePickerTextField to compose

--- app/src/main/java/com/combah/travel2/extensions/Date.kt
+++ app/src/main/java/com/combah/travel2/extensions/Date.kt
@@ -2,7 +2,11 @@ package com.combah.travel2.extensions
 
 import java.text.DateFormat
 import java.text.SimpleDateFormat
-import java.util.*
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+import java.util.concurrent.TimeUnit
 
 fun Date.asCalendar(): Calendar = Calendar.getInstance().apply { time = this@asCalendar }
 
@@ -10,18 +14,43 @@ fun dateFromString(value: String, format: String? = "yyyy-MM-dd'T'HH:mm"): Date?
     return SimpleDateFormat(format, Locale.getDefault()).parse(value)
 }
 
-fun Date.format(style: Int = DateFormat.MEDIUM): String {
-    return SimpleDateFormat.getDateInstance(style).format(this)
+fun Date.formatTime(
+    style: Int = DateFormat.MEDIUM,
+    targetTimeZone: TimeZone = TimeZone.getDefault(),
+    locale: Locale = Locale.getDefault(),
+): String {
+    return SimpleDateFormat.getTimeInstance(style, locale).apply {
+        timeZone = targetTimeZone
+    }
+        .format(this)
 }
 
-fun dateFrom(timestamp: Long, originTimezone: TimeZone, targetTimeZone: TimeZone = TimeZone.getDefault()): Date {
-    val originCalendar = Calendar.getInstance(originTimezone)
-    originCalendar.timeInMillis = timestamp
-    val targetCalendar = Calendar.getInstance(targetTimeZone)
-    targetCalendar.set(
-        originCalendar.get(Calendar.YEAR),
-        originCalendar.get(Calendar.MONTH),
-        originCalendar.get(Calendar.DAY_OF_MONTH),
-    )
-    return targetCalendar.time
+fun Date.format(
+    style: Int = DateFormat.MEDIUM,
+    includeTime: Boolean = false,
+    targetTimeZone: TimeZone = TimeZone.getDefault(),
+): String {
+    return if (includeTime) {
+        SimpleDateFormat.getDateTimeInstance(style, style)
+    } else {
+        SimpleDateFormat.getDateInstance(style)
+    }.apply {
+        timeZone = targetTimeZone
+    }
+        .format(this)
 }
+
+val Date.midnightTime: Long
+    get() = time - (time % TimeUnit.DAYS.toMillis(1))
+
+private val Date.timeWithoutDays: Long
+    get() = time - (time / TimeUnit.DAYS.toMillis(1))
+        .let { TimeUnit.DAYS.toMillis(it) }
+
+val Date.hour: Int
+    get() = (timeWithoutDays / TimeUnit.HOURS.toMillis(1)).toInt()
+
+val Date.minute: Int
+    get() = (timeWithoutDays % TimeUnit.HOURS.toMillis(1)).let {
+        TimeUnit.MILLISECONDS.toMinutes(it)
+    }.toInt()
--- app/src/main/java/com/combah/travel2/ui/trip/creation/composable/DatePickerTextField.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/creation/composable/DatePickerTextField.kt
@@ -1,23 +1,47 @@
 package com.combah.travel2.ui.trip.creation.composable
 
-import androidx.appcompat.app.AppCompatActivity
+import android.text.format.DateFormat.is24HourFormat
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDefaults
+import androidx.compose.material3.DatePickerState
+import androidx.compose.material3.DisplayMode
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TimePicker
+import androidx.compose.material3.TimePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import com.combah.travel2.extensions.dateFrom
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.combah.travel2.extensions.format
-import com.google.android.material.datepicker.CalendarConstraints
-import com.google.android.material.datepicker.DateValidatorPointForward
-import com.google.android.material.datepicker.MaterialDatePicker
+import com.combah.travel2.extensions.formatTime
+import com.combah.travel2.extensions.hour
+import com.combah.travel2.extensions.midnightTime
+import com.combah.travel2.extensions.minute
+import java.text.DateFormat
 import java.util.*
+import java.util.concurrent.TimeUnit
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerTextField(
     onDateSelected: (Date) -> Unit,
@@ -26,10 +50,27 @@ fun DatePickerTextField(
     minDate: Date? = null,
     label: String? = null,
 ) {
-    val context = LocalContext.current
+    val openDialog = remember {
+        mutableStateOf(false)
+    }
+    val dateTimePickerState = rememberDateTimePickerState(minDate, date)
+    when {
+        openDialog.value -> DateTimePickerDialog(
+            state = dateTimePickerState,
+            onDismiss = { openDialog.value = false },
+            onDateSelected = { timestamp ->
+                openDialog.value = false
+                onDateSelected(Date(timestamp))
+            },
+        )
+    }
     Box(modifier = modifier) {
         OutlinedTextField(
-            value = date?.format() ?: "",
+            value = date?.format(
+                style = DateFormat.SHORT,
+                includeTime = true,
+                targetTimeZone = TimeZone.getTimeZone("UTC"),
+            ) ?: "",
             onValueChange = {},
             label = { Text(text = label ?: "") },
             readOnly = true,
@@ -38,28 +79,193 @@ fun DatePickerTextField(
             modifier = Modifier
                 .matchParentSize()
                 .alpha(0f)
-                .clickable(onClick = {
-                    val picker =
-                        MaterialDatePicker.Builder
-                            .datePicker()
-                            .setCalendarConstraints(
-                                CalendarConstraints
-                                    .Builder()
-                                    .setValidator(
-                                        DateValidatorPointForward.from(
-                                            minDate?.time ?: 0L
-                                        )
-                                    )
-                                    .build()
-                            )
-                            .setSelection(date?.time)
-                            .build()
-                    picker.addOnPositiveButtonClickListener { timestamp ->
-                        onDateSelected(dateFrom(timestamp, TimeZone.getTimeZone("UTC")))
-                    }
-                    picker.show((context as AppCompatActivity).supportFragmentManager, picker.toString())
-                }),
+                .clickable(onClick = { openDialog.value = true }),
         )
     }
+}
 
+@VisibleForTesting
+enum class PickerStep {
+    Date, Time,
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@VisibleForTesting
+class DateTimePickerState(
+    val minDate: Date? = null,
+    val datePickerState: DatePickerState,
+    val timePickerState: TimePickerState,
+) {
+
+    constructor(
+        minDate: Date?,
+        date: Date?,
+        is24Hour: Boolean,
+    ) : this(
+        minDate,
+        DatePickerState(
+            date?.time,
+            minDate?.time,
+            DatePickerDefaults.YearRange,
+            DisplayMode.Picker,
+        ),
+        TimePickerState(
+            initialHour = date?.hour ?: 0,
+            initialMinute = date?.minute ?: 0,
+            is24Hour = is24Hour,
+        ),
+    )
+
+    private val stepState = mutableStateOf(PickerStep.Date)
+    private val selectedDateMillis
+        get() = datePickerState.selectedDateMillis
+
+    private val selectedHour
+        get() = timePickerState.hour
+
+    private val selectedMinutes
+        get() = timePickerState.minute
+
+    var step: PickerStep
+        get() = stepState.value
+        set(value) {
+            stepState.value = value
+        }
+
+    val selectedTimestamp: Long?
+        get() {
+            val dateTimestamp = selectedDateMillis ?: return null
+            val hourInMillis = TimeUnit.HOURS.toMillis(selectedHour.toLong())
+            val minutesInMillis = TimeUnit.MINUTES.toMillis(selectedMinutes.toLong())
+            return dateTimestamp + hourInMillis + minutesInMillis
+        }
+
+    val dateConfirmEnabled: Boolean
+        get() = selectedDateMillis != null
+
+    val timeConfirmEnabled: Boolean
+        get() = validateTime(minDate?.time, selectedTimestamp ?: 0L)
+
+    val showTimeErrorMessage: Boolean
+        get() = !timeConfirmEnabled
+}
+
+@Composable
+private fun rememberDateTimePickerState(
+    minDate: Date? = null,
+    date: Date? = null,
+    is24Hour: Boolean = is24HourFormat(LocalContext.current),
+): DateTimePickerState = remember(minDate, date, is24Hour) {
+    DateTimePickerState(
+        minDate,
+        date,
+        is24Hour,
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+private fun DateTimePickerDialog(
+    state: DateTimePickerState,
+    onDateSelected: (Long) -> Unit,
+    onDismiss: () -> Unit,
+) {
+    when (state.step) {
+        PickerStep.Date -> ConfirmationDialog(
+            onConfirm = { state.step = PickerStep.Time },
+            onDismiss = onDismiss,
+            buttonEnabled = state.dateConfirmEnabled
+        ) {
+            DatePicker(
+                state = state.datePickerState,
+                dateValidator = { validateTime(state.minDate?.midnightTime?.minus(1), it) },
+            )
+        }
+
+        PickerStep.Time -> ConfirmationDialog(
+            onConfirm = {
+                state.selectedTimestamp?.let(onDateSelected)
+            },
+            onDismiss = { state.step = PickerStep.Date },
+            buttonEnabled = state.timeConfirmEnabled,
+            errorMessage = if (state.showTimeErrorMessage) {
+                state.minDate?.formatTime(
+                    style = DateFormat.SHORT,
+                    targetTimeZone = TimeZone.getTimeZone("UTC"),
+                )
+            } else {
+                null
+            }?.let { "Please select a time after $it" },
+        ) {
+            TimePicker(
+                modifier = Modifier.padding(top = 16.dp),
+                state = state.timePickerState,
+            )
+        }
+    }
+}
+
+@Composable
+fun ConfirmationDialog(
+    onConfirm: () -> Unit,
+    onDismiss: () -> Unit,
+    buttonEnabled: Boolean,
+    errorMessage: String? = null,
+    content: @Composable () -> Unit,
+) {
+    Dialog(
+        onDismissRequest = onDismiss, properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            shape = MaterialTheme.shapes.medium,
+        ) {
+            Column(modifier = Modifier.fillMaxWidth()) {
+                Box(modifier = Modifier.align(Alignment.CenterHorizontally)) {
+                    content()
+                }
+                Box(
+                    contentAlignment = Alignment.Center, modifier = Modifier
+                        .height(32.dp)
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                ) {
+                    errorMessage?.let {
+                        Text(
+                            text = it,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+                TextButton(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.End),
+                    enabled = buttonEnabled,
+                    onClick = onConfirm,
+                ) {
+                    Text("Confirm")
+                }
+            }
+        }
+    }
+}
+
+private fun validateTime(minDateTimestamp: Long?, time: Long) =
+    minDateTimestamp == null || time > minDateTimestamp
+
+@Composable
+@Preview
+fun DateTimePickerDialogPreview() {
+    MaterialTheme {
+        val dateTimePickerState = rememberDateTimePickerState(
+            minDate = Date(1700076900000),
+            date = Date(1700076900000),
+        )
+        dateTimePickerState.step = PickerStep.Time
+        DateTimePickerDialog(state = dateTimePickerState, onDismiss = {}, onDateSelected = {})
+    }
 }
