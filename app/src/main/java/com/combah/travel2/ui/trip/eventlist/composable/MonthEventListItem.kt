36488235833d1350ccaafe58886ff861ceb736d7
Rodrigo Santos <rodrigo.santos01@gmail.com>
2023-11-19 19:26:17 -0500

    Using ListItem component for TripDetails items

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/MonthEventListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/MonthEventListItem.kt
@@ -1,16 +1,10 @@
 package com.combah.travel2.ui.trip.eventlist.composable
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.combah.travel2.R
 import com.combah.travel2.ui.data.MonthEvent
 import com.combah.travel2.ui.theme.AppTheme
@@ -18,21 +12,15 @@ import java.text.DateFormatSymbols
 
 @Composable
 fun MonthEventListItem(event: MonthEvent) {
-    Box(
-        modifier = Modifier
-            .background(MaterialTheme.colorScheme.surface)
-            .fillMaxWidth()
-    ) {
+    ListItem(headlineContent = {
         Text(
             text = stringResource(
                 R.string.month_event_title,
                 DateFormatSymbols.getInstance().months[event.month],
                 event.year
-            ),
-            modifier = Modifier
-                .padding(all = 16.dp)
+            )
         )
-    }
+    })
 }
 
 @Composable
