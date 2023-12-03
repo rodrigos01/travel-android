65ae1bb607b91e330446a8130f1a42c6ccf50934
Rodrigo Santos <rodrigo.santos01@gmail.com>
2023-12-03 13:39:17 -0500

    Place item redesign

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/PlaceEventListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/PlaceEventListItem.kt
@@ -1,55 +1,85 @@
 package com.combah.travel2.ui.trip.eventlist.composable
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil.compose.rememberAsyncImagePainter
+import com.combah.travel2.R
 import com.combah.travel2.ui.theme.AppTheme
 
 @Composable
-fun PlaceEventListItem(imageUrl: String, placeName: String) {
-    BoxWithConstraints {
-        AsyncImage(
-            model = imageUrl,
-            contentDescription = "Place Description",
-            modifier = Modifier
+fun PlaceEventListItem(imageUrl: String, placeName: String, startDate: String, endDate: String) {
+    Box(
+        Modifier
+            .background(color = MaterialTheme.colorScheme.surface)
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        BoxWithConstraints(
+            Modifier
+                .wrapContentHeight()
                 .fillMaxWidth()
-                .aspectRatio(2.33f),
-            contentScale = ContentScale.FillWidth
-        )
-        Text(
-            text = placeName,
-            color = Color.White,
-            style = MaterialTheme.typography.headlineSmall,
-            modifier = Modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomStart)
-                .background(
-                    Brush.verticalGradient(
-                        listOf(Color.Transparent, Color(0xDD000000))
-                    )
+                .clip(RoundedCornerShape(12.dp))
+                .background(color = MaterialTheme.colorScheme.tertiary)
+        ) {
+            Image(
+                painter = rememberAsyncImagePainter(model = imageUrl),
+                contentDescription = "Place Description",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .aspectRatio(2.33f),
+                contentScale = ContentScale.FillWidth
+            )
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center,
+                modifier = Modifier
+                    .matchParentSize()
+                    .padding(horizontal = 16.dp, vertical = 8.dp)
+            ) {
+                Text(
+                    text = placeName,
+                    color = Color.White,
+                    style = MaterialTheme.typography.displayMedium,
                 )
-                .padding(horizontal = 16.dp, vertical = 8.dp)
-        )
+                Text(
+                    text = stringResource(id = R.string.place_item_subtitle, startDate, endDate),
+                    color = Color.White,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
+        }
     }
+
 }
 
 @Composable
 @Preview
 fun PlaceEventListItemPreview() {
     AppTheme {
-        PlaceEventListItem("","New York City")
+        PlaceEventListItem(
+            "https://encrypted-tbn1.gstatic.com/licensed-image?q=tbn:ANd9GcRDbATmnXe3E928q15EUN0TxheIKg6QwtjNKMqMTH297oQ5AsfFKehPfGsyzbKTPrKkybBhdbp_CWtuZxf6Sx6ld8xxqYty1UaqhJfmZQw",
+            "New York City",
+            "May 11",
+            "May 21",
+        )
     }
 }
