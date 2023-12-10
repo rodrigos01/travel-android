84977632ccfd5533613c401dca419ff511b66e2e
Rodrigo Santos <rodrigo.santos01@gmail.com>
2023-12-10 14:14:59 -0500

    Separating add plan logic into Use Cases

---
+++ app/src/test/java/com/combah/travel2/ui/trip/viewmodel/AddLodgingUseCaseTest.kt
@@ -0,0 +1,46 @@
+package com.combah.travel2.ui.trip.viewmodel
+
+import com.combah.travel2.extensions.TimeFormatter
+import com.combah.travel2.model.data.Time
+import com.combah.travel2.test.assertType
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.stub
+import org.assertj.core.api.Assertions
+import org.junit.Test
+import kotlin.contracts.ExperimentalContracts
+
+@OptIn(ExperimentalContracts::class)
+class AddLodgingUseCaseTest {
+    private val formatter: TimeFormatter = mock()
+    private val subject = AddLodgingUseCase(formatter)
+
+    @Test
+    fun `created item should be initialized empty`() {
+        val addedItem = subject.createItem(mock())
+        assertType<AddLodgingUseCase.AddLodgingItem>(addedItem)
+        Assertions.assertThat(addedItem.name).isNull()
+        Assertions.assertThat(addedItem.checkOutDayOfMonth).isNull()
+        Assertions.assertThat(addedItem.checkOutDayOfWeek).isNull()
+        Assertions.assertThat(addedItem.checkOutTime).isNull()
+    }
+
+    @Test
+    fun `created item should be initialized with initial time as departure`() {
+        val initialTime: Time = mock()
+        formatter.stub {
+            on { timeString(initialTime) } doReturn "6:15"
+        }
+        val addedItem = subject.createItem(initialTime)
+        assertType<AddLodgingUseCase.AddLodgingItem>(addedItem)
+        Assertions.assertThat(addedItem.checkInTime).isEqualTo("6:15")
+    }
+
+    @Test
+    fun `created pending data should be initialized with initial time as departure`() {
+        val initialTime: Time = mock()
+        val pendingData = subject.createPendingData("id", initialTime)
+        assertType<AddLodgingUseCase.PendingLodging>(pendingData)
+        Assertions.assertThat(pendingData.checkIn).isEqualTo(initialTime)
+    }
+}
