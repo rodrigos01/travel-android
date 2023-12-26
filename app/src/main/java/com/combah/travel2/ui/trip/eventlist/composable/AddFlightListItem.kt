cdfde16910f499629e8488cd32f31e7a43190d4c
Rodrigo Santos <rodrigo.santos01@gmail.com>
2023-12-26 01:44:19 -0500

    Add Flight UI Item skeleton

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/AddFlightListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/AddFlightListItem.kt
@@ -1,8 +1,32 @@
 package com.combah.travel2.ui.trip.eventlist.composable
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.combah.travel2.ui.theme.AppTheme
 
 @Composable
-fun AddFlightListItem() {
+fun AddFlightListItem(
+    departureTime: String,
+    airportFromName: String? = null,
+    airportFromSearchResults: List<String> = emptyList(),
+    arrivalTime: String,
+    arrivalDayOfMonth: String,
+    arrivalDayOfWeek: String,
+    airportToName: String? = null,
+    airportToSearchResults: List<String> = emptyList(),
+) {
 
+}
+
+@Composable
+@Preview
+fun AddFlightListItemPreview() {
+    AppTheme {
+        AddFlightListItem(
+            departureTime = "6:15 PM",
+            arrivalTime = "set arrival time",
+            arrivalDayOfMonth = "15",
+            arrivalDayOfWeek = "Wed",
+        )
+    }
 }
