6ea5bccbbff1dd17db9666cf8ac103b123c04c93
Rodrigo Santos <rodrigo.santos01@gmail.com>
2024-02-11 22:13:18 -0500

    Suggesting airports from airport from text field

--- app/src/main/java/com/combah/travel2/model/repository/AddFlightRepository.kt
+++ app/src/main/java/com/combah/travel2/model/repository/AddFlightRepository.kt
@@ -1,8 +1,21 @@
 package com.combah.travel2.model.repository
 
 import com.combah.travel2.model.data.Airport
+import com.combah.travel2.model.network.ApiResponse
+import com.combah.travel2.model.network.httpClient
+import com.combah.travel2.model.network.toAppDataModel
+import io.ktor.client.call.body
+import io.ktor.client.request.get
 
 class AddFlightRepository {
-
-    suspend fun autocomplete(query: String): List<Airport> = emptyList()
+    suspend fun autocomplete(query: String): List<Airport> {
+        val response: ApiResponse.AirportAutoComplete =
+            httpClient().get("http://10.0.2.2:5000/places/autocomplete") {
+                url {
+                    parameters.append("query", query)
+                    parameters.append("types", "airport")
+                }
+            }.body()
+        return response.data.map { it.toAppDataModel() }
+    }
 }
