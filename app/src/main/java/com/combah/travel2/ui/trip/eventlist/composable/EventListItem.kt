45716801dc7a0b2679e28ccd344d72af9ecc2bc1
Rodrigo dos Santos <rodrigos01@fb.com>
2024-03-04 19:52:43 -0500

    Fixing list item icon colors

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/EventListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/EventListItem.kt
@@ -6,21 +6,20 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import com.combah.travel2.R
+import com.combah.travel2.ui.theme.AppTheme
 
 @Composable
 fun EventListItem(
@@ -52,6 +51,7 @@ fun EventListItem(
                 leadingContent = {
                     Image(
                         painter = painterResource(id = icon),
+                        colorFilter = ColorFilter.tint(LocalContentColor.current),
                         contentDescription = "Event Icon",
                     )
                 },
@@ -74,9 +74,9 @@ fun EventListItem(
 }
 
 @Composable
-@Preview
+@PreviewLightDark
 fun EventListItemPreview() {
-    MaterialTheme {
+    AppTheme {
         EventListItem(
             showDate = true,
             icon = R.drawable.ic_flight_takeoff_black_24dp,
