3e4e9beb0358c46eb8792d32d7f30a5cf948634b
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-02-17 17:59:54 -0500

    Lodging Search list item component

--- app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/StartEndAddPlanListItem.kt
+++ app/src/main/java/com/combah/travel2/ui/trip/eventlist/composable/StartEndAddPlanListItem.kt
@@ -1,34 +1,14 @@
 package com.combah.travel2.ui.trip.eventlist.composable
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.combah.travel2.model.data.Time
 import com.combah.travel2.ui.theme.AppTheme
 import com.combah.travel2.ui.trip.creation.composable.AddPlanType
-import com.combah.travel2.ui.trip.creation.composable.ConfirmationDialog
-import com.combah.travel2.ui.trip.creation.composable.TypeSelectorButton
 import java.util.TimeZone
 
 @Composable
@@ -72,37 +52,20 @@ fun StartEndAddPlanListItem(
     deleteButtonEnabled: Boolean,
     onDeleteConfirmed: () -> Unit,
 ) {
-    var showDeleteConfirmation by remember { mutableStateOf(false) }
-    if (showDeleteConfirmation) {
-        ConfirmationDialog(
-            onConfirm = onDeleteConfirmed,
-            onDismiss = { showDeleteConfirmation = false },
-            confirmButtonLabel = "Delete",
-            confirmButtonColors = ButtonDefaults.textButtonColors(contentColor = MaterialTheme.colorScheme.error),
-            dismissButtonLabel = "Cancel"
-        ) {
-            Text("Delete ${initialType.label}?")
-        }
-    }
-    Column(
+    AddPlanListItem(
+        initialType,
+        onTypeSelected,
+        typeSelectionEnabled,
+        deleteButtonEnabled,
+        onDeleteConfirmed,
+        primaryButtonEnabled = saveButtonEnabled,
+        primaryButtonLabel = "Save",
+        onPrimaryButtonTapped = onSaveButtonTapped,
+        secondaryButtonLabel = "Cancel",
+        onSecondaryButtonTapped = onCancelButtonTapped,
         modifier = Modifier
-            .background(MaterialTheme.colorScheme.surface)
             .padding(top = 8.dp, bottom = 16.dp)
     ) {
-        Row {
-            TypeSelectorButton(
-                initialType = initialType,
-                onOptionSelected = onTypeSelected,
-                enabled = typeSelectionEnabled,
-            )
-            if (deleteButtonEnabled) {
-                Spacer(modifier = Modifier.weight(1F))
-                TextButton(onClick = { showDeleteConfirmation = true }) {
-                    Icon(Icons.Filled.Delete, contentDescription = null)
-                    Text("Delete")
-                }
-            }
-        }
         AddPlanRow(
             title = startTitle,
             minTime = minStartTime,
@@ -137,17 +100,6 @@ fun StartEndAddPlanListItem(
             searchResults = endSearchResults,
             searchResultTapped = endSearchResultTapped,
         )
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.padding(top = 8.dp, end = 16.dp).align(Alignment.End)
-        ) {
-            OutlinedButton(onClick = { onCancelButtonTapped() }) {
-                Text("Cancel")
-            }
-            Button(onClick = { onSaveButtonTapped() }, enabled = saveButtonEnabled) {
-                Text("Save")
-            }
-        }
     }
 
 }
