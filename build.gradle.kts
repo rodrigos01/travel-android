495e3f91fd94b0edad6aa6f4ca84da958d1ecd5f
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-03-17 22:44:19 -0400

    Adding firebase app distribution

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,6 @@ plugins {
     id("androidx.navigation.safeargs.kotlin") version "2.8.6" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     kotlin("plugin.serialization") version "2.1.10"
+
+    id("com.google.firebase.appdistribution") version "5.1.1" apply false
 }
