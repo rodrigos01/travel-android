54d82bc868902896a20a9401dd9a879dc9af659f
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-03-29 19:12:45 -0400

    reviews fixes

--- app/src/main/java/travel/vola/android/ui/lodgingsearch/composable/LodgingReviewItem.kt
+++ app/src/main/java/travel/vola/android/ui/lodgingsearch/composable/LodgingReviewItem.kt
@@ -8,7 +8,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.ButtonDefaults
@@ -24,24 +24,46 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
+import coil.decode.SvgDecoder
+import coil.request.ImageRequest
 import travel.vola.android.common.ui.preview.loremIpsum
 import travel.vola.android.extensions.Time
+import travel.vola.android.extensions.dateString
+import travel.vola.android.extensions.monthAndYearString
 import travel.vola.android.ui.lodgingsearch.state.LodgingReviewState
 import travel.vola.android.ui.theme.AppTheme
+import java.text.DateFormat
 
 @Composable
 fun LodgingReviewItem(state: LodgingReviewState) {
     Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            LodgingRating(state.rating)
-            Text(state.title, style = MaterialTheme.typography.titleMedium)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    state.title,
+                    style = MaterialTheme.typography.titleMedium,
+                )
+                Text(
+                    state.reviewTime.dateString(style = DateFormat.LONG),
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+            Image(
+                painter = rememberAsyncImagePainter(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(state.ratingImageUrl)
+                        .decoderFactory(SvgDecoder.Factory())
+                        .build()
+                ),
+                contentDescription = null,
+                modifier = Modifier.height(24.dp)
+            )
         }
         var expanded by remember { mutableStateOf(true) }
         var hasMoreText by remember { mutableStateOf(false) }
@@ -50,31 +72,39 @@ fun LodgingReviewItem(state: LodgingReviewState) {
             maxLines = if (expanded) Int.MAX_VALUE else 6,
             overflow = TextOverflow.Ellipsis,
             onTextLayout = {
-                if (!hasMoreText && it.lineCount > 20) {
+                if (!hasMoreText && it.lineCount > 6) {
                     hasMoreText = true
                     expanded = false
                 }
             },
             modifier = Modifier.animateContentSize(),
         )
-
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.spacedBy(8.dp),
-            modifier = Modifier.height(ButtonDefaults.MinHeight)
+            modifier = Modifier.heightIn(min = ButtonDefaults.MinHeight)
         ) {
             Image(
                 painter = rememberAsyncImagePainter(state.authorAvatarUrl),
                 contentDescription = null,
                 modifier = Modifier
-                    .size(24.dp)
+                    .size(36.dp)
                     .clip(CircleShape)
                     .background(MaterialTheme.colorScheme.surfaceVariant)
             )
-            Text(state.authorName, style = MaterialTheme.typography.labelLarge)
-            state.authorLocation?.let { location ->
+            Column {
+                Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                    Text(state.authorName, style = MaterialTheme.typography.labelLarge)
+                    state.authorLocation?.let { location ->
+                        Text(
+                            location,
+                            color = MaterialTheme.colorScheme.onSurfaceVariant,
+                            style = MaterialTheme.typography.bodyMedium
+                        )
+                    }
+                }
                 Text(
-                    location,
+                    "Traveled on ${state.tripDate.monthAndYearString()}",
                     color = MaterialTheme.colorScheme.onSurfaceVariant,
                     style = MaterialTheme.typography.bodyMedium
                 )
@@ -88,6 +118,7 @@ fun LodgingReviewItem(state: LodgingReviewState) {
                 }
             }
         }
+
     }
 }
 
@@ -99,14 +130,14 @@ fun LodgingReviewItemPreview() {
             LodgingReviewItem(
                 state = LodgingReviewState(
                     rating = 4.5,
-                    ratingImageUrl = "",
+                    ratingImageUrl = "https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/s5.0-66827-5.svg",
                     tripDate = Time("2023-08-15T00:00 GMT"),
                     reviewTime = Time("2023-08-31T10:52 GMT"),
-                    authorAvatarUrl = null,
+                    authorAvatarUrl = "https://media-cdn.tripadvisor.com/media/photo-l/1a/f6/e4/2d/default-avatar-2020-48.jpg",
                     authorName = "Author",
                     authorLocation = "Author Location",
                     review = loremIpsum(),
-                    title = "A lovely stay",
+                    title = "A Super long review title that will span multiple lines for sure",
                 )
             )
         }
