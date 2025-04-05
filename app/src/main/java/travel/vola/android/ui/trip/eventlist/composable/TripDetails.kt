e368c5ae50ddd7052ccc5e280a775ad0f6afa6d5
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-04-05 09:44:36 -0400

    trip details reponsive layout

--- app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/TripDetails.kt
+++ app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/TripDetails.kt
@@ -4,6 +4,11 @@ import androidx.compose.animation.core.VisibilityThreshold
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -25,18 +30,29 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import androidx.window.core.layout.WindowSizeClass
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import travel.vola.android.model.PlaceRepository
 import travel.vola.android.model.data.Identifiable
 import travel.vola.android.model.repository.mock.MockTripRepository
@@ -53,11 +69,38 @@ import travel.vola.android.ui.trip.state.TripItemState.HotelCheckOutItemState
 import travel.vola.android.ui.trip.state.TripItemState.MonthItemState
 import travel.vola.android.ui.trip.state.TripItemState.PlaceItemState
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TripDetails(
     viewModel: travel.vola.android.ui.trip.viewmodel.TripViewModel,
     navController: NavController,
+) {
+    val windowSizeClass = currentWindowAdaptiveInfo().windowSizeClass
+    val isLargeScreen =
+        windowSizeClass.isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_MEDIUM_LOWER_BOUND)
+    if (isLargeScreen) {
+        val maxListWidth = when {
+            windowSizeClass.isWidthAtLeastBreakpoint(WindowSizeClass.WIDTH_DP_EXPANDED_LOWER_BOUND) -> 400.dp
+            else -> 320.dp
+        }
+        Row(Modifier.fillMaxWidth()) {
+            List(
+                viewModel, navController, modifier = Modifier.widthIn(max = maxListWidth)
+            )
+            Map(modifier = Modifier.weight(1F))
+        }
+    } else {
+        List(
+            viewModel, navController
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun List(
+    viewModel: travel.vola.android.ui.trip.viewmodel.TripViewModel,
+    navController: NavController,
+    modifier: Modifier = Modifier,
 ) {
     val state by viewModel.viewState.collectAsStateWithLifecycle()
     var isInEditMode by remember {
@@ -85,7 +128,7 @@ fun TripDetails(
         }
     }
     Scaffold(
-        containerColor = MaterialTheme.colorScheme.surface,
+        modifier = modifier,
         topBar = {
             TopAppBar(title = {
                 if (isInEditMode) {
@@ -136,12 +179,9 @@ fun TripDetails(
                 }
             })
         },
-        modifier = Modifier.background(MaterialTheme.colorScheme.surface),
     ) { paddingValues ->
         LazyColumn(contentPadding = paddingValues) {
-            items(
-                state.items,
-                key = { (it as? Identifiable)?.id ?: it.hashCode() }) { event ->
+            items(state.items, key = { (it as? Identifiable)?.id ?: it.hashCode() }) { event ->
                 Box(
                     modifier = Modifier.animateItem(placementSpec = spring(visibilityThreshold = IntOffset.VisibilityThreshold))
                 ) {
@@ -219,8 +259,7 @@ private fun TripDetailItem(
             showDivider = false,
             onAddButtonClick = { viewModel.addButtonTapped(event.id) })
 
-        is TripItemState.EmptyAddPlanItemState -> EmptyAddPlanListItem(
-            showDivider = event.showDivider,
+        is TripItemState.EmptyAddPlanItemState -> EmptyAddPlanListItem(showDivider = event.showDivider,
             onAddButtonClick = { viewModel.addButtonTapped(event.id) })
 
         is AddPlanItemState -> AddPlanListItem(
@@ -231,7 +270,32 @@ private fun TripDetailItem(
 }
 
 @Composable
+private fun Details(modifier: Modifier = Modifier) {
+
+}
+
+@Composable
+private fun Map(modifier: Modifier = Modifier) {
+    val cameraPositionState = rememberCameraPositionState()
+    GoogleMap(
+        cameraPositionState = cameraPositionState,
+        uiSettings = MapUiSettings(
+            indoorLevelPickerEnabled = false,
+            myLocationButtonEnabled = false,
+            rotationGesturesEnabled = false,
+            tiltGesturesEnabled = false,
+        ),
+        modifier = modifier
+            .background(color = MaterialTheme.colorScheme.surfaceContainer)
+    ) {
+
+    }
+}
+
+@Composable
 @Preview
+@Preview(device = "spec:parent=pixel_tablet,orientation=portrait")
+@Preview(device = "id:pixel_tablet")
 fun TripDetailsPreview() {
     AppTheme(dynamicColor = false) {
         val navController = rememberNavController()
