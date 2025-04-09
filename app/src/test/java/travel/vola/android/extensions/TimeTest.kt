b91414cf7f9d91e36466bf4a91c12ee5dd09cdca
Rodrigo dos Santos <rodrigos01@fb.com>
2025-04-09 07:32:12 -0400

    Fixing firebase parser test

--- app/src/test/java/travel/vola/android/extensions/TimeTest.kt
+++ app/src/test/java/travel/vola/android/extensions/TimeTest.kt
@@ -9,10 +9,10 @@ class TimeTest {
     fun `String to Time should create Time object with correct values and timezone`() {
         val time = Time("2024-05-21T16:50 +0200")
         Assertions.assertThat(time.year).isEqualTo(2024)
-        Assertions.assertThat(time.month).isEqualTo(5)
+        Assertions.assertThat(time.monthValue).isEqualTo(5)
         Assertions.assertThat(time.dayOfMonth).isEqualTo(21)
         Assertions.assertThat(time.hour).isEqualTo(16)
         Assertions.assertThat(time.minute).isEqualTo(50)
-        Assertions.assertThat(time.zone).isEqualTo(ZoneId.of("GMT+2"))
+        Assertions.assertThat(time.zone).isEqualTo(ZoneId.of("+02:00"))
     }
 }
