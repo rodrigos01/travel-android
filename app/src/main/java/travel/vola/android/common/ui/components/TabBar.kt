e382955c306f5e22d653a5a3888b949aacaa88a5
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-05-04 09:19:18 -0400

    separating tab bar from tabbed host

---
+++ app/src/main/java/travel/vola/android/common/ui/components/TabBar.kt
@@ -0,0 +1,129 @@
+package travel.vola.android.common.ui.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement.spacedBy
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.zIndex
+import kotlin.math.roundToInt
+
+object TabBar {
+    class Tab(
+        val id: String,
+        val selected: Boolean = false,
+        val icon: @Composable () -> Unit = {},
+        val title: @Composable () -> Unit = {},
+        val modifier: Modifier = Modifier,
+    )
+
+    class Builder {
+        private val tabs = mutableListOf<Tab>()
+
+        fun tab(
+            id: String,
+            selected: Boolean = false,
+            icon: @Composable () -> Unit = {},
+            title: @Composable () -> Unit = {},
+            modifier: Modifier = Modifier,
+        ) = tabs.add(Tab(id, selected, icon, title, modifier))
+
+        fun build(): List<Tab> = tabs
+    }
+}
+
+@Composable
+fun TabBar(
+    modifier: Modifier = Modifier,
+    tabBarListState: LazyListState = rememberLazyListState(),
+    onTabClick: (String) -> Unit = {},
+    builder: TabBar.Builder.() -> Unit,
+) {
+    val tabs = TabBar.Builder().apply(builder).build()
+    LazyRow(
+        state = tabBarListState,
+        modifier = modifier
+            .background(
+                MaterialTheme.colorScheme.surfaceContainer
+            ),
+    ) {
+        items(tabs, key = { it.id }) { tab ->
+            val selected = tab.selected
+            val tabSize = with(LocalDensity.current) { 48.dp.toPx() }
+            var targetOffsetX by remember { mutableFloatStateOf(-tabSize) }
+            var zIndex by remember { mutableFloatStateOf(-1F) }
+            val offsetX by animateFloatAsState(
+                targetOffsetX,
+                finishedListener = { zIndex = 0F },
+            )
+            FilledTonalButton(
+                onClick = { onTabClick(tab.id) },
+                colors = if (selected) {
+                    ButtonDefaults.filledTonalButtonColors()
+                } else {
+                    ButtonDefaults.outlinedButtonColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = tab.modifier
+                    .onPlaced {
+                        targetOffsetX = 0F
+                    }
+                    .offset {
+                        IntOffset(x = offsetX.roundToInt(), y = 0)
+                    }
+                    .zIndex(zIndex)
+                    .animateItem()
+            ) {
+                Row(
+                    horizontalArrangement = spacedBy(8.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    tab.icon()
+                    tab.title()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun TabBarPreview() {
+    TabBar {
+        tab(
+            "home",
+            selected = true,
+            icon = { Icon(Icons.Outlined.Home, contentDescription = null) },
+            title = { Text("Home") },
+        )
+        tab(
+            "search",
+            icon = { Icon(Icons.Outlined.Search, contentDescription = null) },
+            title = { Text("Search") },
+        )
+    }
+}
--- app/src/main/java/travel/vola/android/common/ui/components/TabbedHost.kt
+++ app/src/main/java/travel/vola/android/common/ui/components/TabbedHost.kt
@@ -1,58 +1,40 @@
 package travel.vola.android.common.ui.components
 
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement.spacedBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.navigationBars
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveableStateHolder
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.onPlaced
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.zIndex
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import travel.vola.android.common.ui.components.TabbedHostScope.Tab
 import travel.vola.android.ui.theme.AppTheme
 import kotlin.math.max
-import kotlin.math.roundToInt
 
 class TabbedHostScope(
-    builder: TabbedGraphBuilder.() -> Unit,
-    val navigate: (String) -> Unit
+    builder: TabbedGraphBuilder.() -> Unit, val navigate: (String) -> Unit
 ) {
     class Tab(
         val id: String,
@@ -65,21 +47,19 @@ class TabbedHostScope(
     val tabs =
         mutableListOf<Tab>().apply { TabbedGraphBuilder().apply(builder).opps.forEach { it() } }
 
-    fun findTab(tabId: String) =
-        tabs.firstOrNull { it.id == tabId } ?: Tab(
-            tabId,
-            icon = {
-                Icon(
-                    Icons.Filled.Warning,
-                    contentDescription = null
-                )
-            },
-            content = { Text("No Tab Found for id $tabId") }
-        )
+    fun findTab(tabId: String) = tabs.firstOrNull { it.id == tabId } ?: Tab(
+        tabId,
+        icon = {
+            Icon(
+                Icons.Filled.Warning, contentDescription = null
+            )
+        },
+        content = { Text("No Tab Found for id $tabId") },
+    )
 }
 
 class TabbedGraphBuilder {
-    val opps = mutableListOf<(MutableList<Tab>.() -> Unit)>()
+    val opps = mutableListOf<(MutableList<TabbedHostScope.Tab>.() -> Unit)>()
     fun tab(
         tabId: String,
         icon: @Composable () -> Unit = {},
@@ -87,7 +67,17 @@ class TabbedGraphBuilder {
         modifier: Modifier = Modifier,
         content: @Composable TabbedHostScope.() -> Unit,
     ) {
-        opps.add { add(Tab(tabId, icon, title, modifier, content)) }
+        opps.add {
+            add(
+                TabbedHostScope.Tab(
+                    tabId,
+                    icon,
+                    title,
+                    modifier,
+                    content
+                )
+            )
+        }
     }
 }
 
@@ -114,52 +104,22 @@ fun TabbedHost(
                 scope.findTab(currentTabId).content(scope)
             }
         }
-        AnimatedVisibility(
-            visible = scope.tabs.size > 1, modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    MaterialTheme.colorScheme.surfaceContainer
-                )
-                .windowInsetsPadding(WindowInsets.navigationBars)
-        ) {
-            LazyRow(
-                state = tabBarListState,
+        AnimatedVisibility(visible = scope.tabs.size > 1) {
+            TabBar(
+                tabBarListState = tabBarListState,
+                onTabClick = { currentTabId = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .windowInsetsPadding(WindowInsets.navigationBars)
             ) {
-                items(scope.tabs, key = { it.id }) { tab ->
-                    val selected = tab.id == currentTabId
-                    val tabSize = with(LocalDensity.current) { 48.dp.toPx() }
-                    var targetOffsetX by remember { mutableFloatStateOf(-tabSize) }
-                    var zIndex by remember { mutableFloatStateOf(-1F) }
-                    val offsetX by animateFloatAsState(
-                        targetOffsetX,
-                        finishedListener = { zIndex = 0F },
+                scope.tabs.forEach {
+                    tab(
+                        it.id,
+                        selected = it.id == currentTabId,
+                        title = it.title,
+                        icon = it.icon,
+                        modifier = it.modifier,
                     )
-                    FilledTonalButton(
-                        onClick = { scope.navigate(tab.id) },
-                        colors = if (selected) {
-                            ButtonDefaults.filledTonalButtonColors()
-                        } else {
-                            ButtonDefaults.outlinedButtonColors(containerColor = MaterialTheme.colorScheme.surfaceContainer)
-                        },
-                        shape = MaterialTheme.shapes.medium,
-                        modifier = tab.modifier
-                            .onPlaced {
-                                targetOffsetX = 0F
-                            }
-                            .offset {
-                                IntOffset(x = offsetX.roundToInt(), y = 0)
-                            }
-                            .zIndex(zIndex)
-                            .animateItem()
-                    ) {
-                        Row(
-                            horizontalArrangement = spacedBy(8.dp),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            tab.icon()
-                            tab.title()
-                        }
-                    }
                 }
             }
         }
