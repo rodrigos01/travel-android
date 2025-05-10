3f9d5a7f74a9cbccff7ef12f32b92124c01e4c5b
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-05-10 11:07:51 -0400

    Updated time picker button for add plan row

--- app/src/main/java/travel/vola/android/ui/trip/creation/composable/TimePickerButton.kt
+++ app/src/main/java/travel/vola/android/ui/trip/creation/composable/TimePickerButton.kt
@@ -1,14 +1,16 @@
 package travel.vola.android.ui.trip.creation.composable
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
@@ -17,28 +19,23 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import travel.vola.android.R
+import travel.vola.android.common.ui.components.Clock
 import travel.vola.android.ui.theme.AppTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TimePickerButton(
+fun TimePickerHost(
     onTimeSelected: (hour: Int, minute: Int) -> Unit,
-    modifier: Modifier = Modifier,
     showTimePickerState: MutableState<Boolean> = remember { mutableStateOf(false) },
     timePickerDialogState: TimePickerDialogState = rememberTimePickerDialogState(),
     content: @Composable () -> Unit,
 ) {
     var showTimePicker: Boolean by showTimePickerState
-    Surface(
-        onClick = { showTimePicker = true },
-        modifier = modifier,
-        content = content,
-    )
+    content()
     if (showTimePicker) {
         TimePickerDialog(
             state = timePickerDialogState,
@@ -59,28 +56,38 @@ fun TimePickerTextButton(
     text: String,
     onTimeSelected: (hour: Int, minute: Int) -> Unit,
     modifier: Modifier = Modifier,
+    contentColor: Color = ButtonDefaults.textButtonColors().contentColor,
+    textStyle: TextStyle = MaterialTheme.typography.labelLarge,
+    iconSize: Dp = 24.dp,
     timePickerDialogState: TimePickerDialogState = rememberTimePickerDialogState(),
     showTimePickerState: MutableState<Boolean> = remember { mutableStateOf(false) },
 ) {
-    TimePickerButton(
+    var showTimePicker: Boolean by showTimePickerState
+    TimePickerHost(
         onTimeSelected,
-        modifier = modifier,
         timePickerDialogState = timePickerDialogState,
         showTimePickerState = showTimePickerState,
     ) {
-        Row {
-            Icon(
-                painter = painterResource(id = R.drawable.clock_baseline_16),
-                contentDescription = null,
-                tint = MaterialTheme.colorScheme.tertiary,
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .padding(end = 4.dp)
-            )
-            Text(
-                text = text,
-                style = TextStyle(color = MaterialTheme.colorScheme.tertiary)
-            )
+        TextButton(
+            onClick = { showTimePicker = true },
+            colors = ButtonDefaults.textButtonColors(contentColor = contentColor),
+            modifier = modifier,
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.Clock,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(iconSize),
+                )
+                Text(
+                    text = text,
+                    style = textStyle,
+                )
+            }
         }
     }
 }
