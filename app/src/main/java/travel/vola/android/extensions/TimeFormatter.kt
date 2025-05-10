ceb01160a186207a23d1b6f41379d05c285778cb
Rodrigo Silva dos Santos <rodrigos.santos01@gmail.com>
2025-05-10 18:43:51 -0400

    Showing only TimedPlaces with end time as Place events in trip details

--- app/src/main/java/travel/vola/android/extensions/TimeFormatter.kt
+++ app/src/main/java/travel/vola/android/extensions/TimeFormatter.kt
@@ -24,7 +24,7 @@ val Time.dayAndMonthString: String
     get() = format(DateTimeFormatter.ofPattern("MMM d"))
 
 val Time.dayOfWeekString: String
-    get() = DateFormatSymbols.getInstance().shortWeekdays[dayOfWeek.value + 1]
+    get() = format(DateTimeFormatter.ofPattern("E"))
 
 val Time.timeString: String
     get() = format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))
