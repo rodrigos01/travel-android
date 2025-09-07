67138f108b1a1546f733cb09a38023766ea13633
Rodrigo <rodrigos.santos01@gmail.com>
2025-09-07 15:40:28 -0400

    Replacing ServiceLocator with ViewModelFactoryDependencies provided through CreationExtras

--- app/src/main/java/travel/vola/android/extensions/ViewModel.kt
+++ app/src/main/java/travel/vola/android/extensions/ViewModel.kt
@@ -1,17 +1,34 @@
 package travel.vola.android.extensions
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.HasDefaultViewModelProviderFactory
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.compose.LocalViewModelStoreOwner
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.plus
 import androidx.lifecycle.viewmodel.viewModelFactory
+import travel.vola.android.di.LocalViewModelCreationExtras
 
 @Composable
-inline fun <reified VM : ViewModel> viewModel(noinline initializer: () -> VM): VM =
-    viewModel(
-        factory = viewModelFactory {
-            initializer {
-                initializer()
-            }
-        }
+inline fun <reified VM : ViewModel> viewModel(
+    factory: ViewModelProvider.Factory,
+    additionalExtras: CreationExtras = CreationExtras.Empty
+): VM {
+    val viewModelStoreOwner = LocalViewModelStoreOwner.current
+    return viewModel(
+        factory = factory,
+        extras = if (viewModelStoreOwner is HasDefaultViewModelProviderFactory) {
+            viewModelStoreOwner.defaultViewModelCreationExtras
+        } else {
+            CreationExtras.Empty
+        } + LocalViewModelCreationExtras.current + additionalExtras,
     )
+}
+
+inline fun <reified VM : ViewModel> viewModelFactory(noinline initializer: CreationExtras.() -> VM): ViewModelProvider.Factory =
+    viewModelFactory {
+        initializer(initializer)
+    }
