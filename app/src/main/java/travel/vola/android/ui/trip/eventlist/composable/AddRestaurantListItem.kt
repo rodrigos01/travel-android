c9cc84f9c0ee499ebb6c5aae0654fc5ecb4c60c3
Rodrigo <rodrigos.santos01@gmail.com>
2025-09-15 07:16:24 -0400

    Add Restaurant list item UI

---
+++ app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/AddRestaurantListItem.kt
@@ -0,0 +1,83 @@
+package travel.vola.android.ui.trip.eventlist.composable
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import travel.vola.android.model.data.Time
+import travel.vola.android.ui.theme.AppTheme
+import travel.vola.android.ui.trip.state.ManualAddLodgingItemState
+import travel.vola.android.ui.trip.state.ManualAddPlanState
+import travel.vola.android.ui.trip.state.ManualStartEndAddPlanState
+import java.time.ZonedDateTime
+
+@Composable
+fun AddRestaurantListItem(
+    uiState: ManualStartEndAddPlanState,
+    onTextChanged: (CharSequence) -> Unit,
+    onUpdated: (
+        dateTime: ZonedDateTime?,
+        timeSelected: Boolean,
+        selectedSearchResultIndex: Int,
+    ) -> Unit,
+) {
+    StartEndAddPlanListItem(
+        uiState = uiState,
+        startTitle = { Text("Start") },
+        startTimeSelectorLabel = "Pick Time",
+        startLabelText = "Location",
+        startPlaceHolder = "Enter Location",
+        onStartTextChanged = onTextChanged,
+        endTitle = {},
+        endTimeSelectorLabel = "",
+        canSetEnd = false,
+        onUpdated = {
+                startDateTime,
+                startTimeSelected,
+                selectedStartSearchResultIndex,
+                endDateTime,
+                endTimeSelected,
+                _,
+            ->
+            onUpdated(
+                startDateTime,
+                startTimeSelected,
+                selectedStartSearchResultIndex
+            )
+        })
+}
+
+@PreviewLightDark
+@Composable
+fun AddRestaurantListItemPreview() {
+    AppTheme {
+        Surface {
+            AddRestaurantListItem(
+                uiState = ManualAddLodgingItemState(
+                    "", Time.now(),
+                    typeSelectionEnabled = false,
+                    startState = ManualAddPlanState(
+                        dateTime = null,
+                        minDateTime = Time.now(),
+                        isTimeSet = false,
+                        dateSelectionEnabled = false,
+                        locationText = null,
+                        searchResults = emptyList()
+                    ),
+                    endState = ManualAddPlanState(
+                        dateTime = null,
+                        minDateTime = Time.now(),
+                        isTimeSet = false,
+                        dateSelectionEnabled = true,
+                        locationText = null,
+                        searchResults = emptyList()
+                    ),
+                    deleteButtonEnabled = true,
+                    saveButtonEnabled = true,
+                ),
+                onTextChanged = {},
+                onUpdated = { _, _, _ -> },
+            )
+        }
+    }
+}
