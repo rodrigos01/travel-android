db7660038ef0b6996432f889d1d3d46cebfeb4fe
Rodrigo <rodrigos.santos01@gmail.com>
2025-12-10 19:26:13 -0500

    implementing basic genAI prompt to get place suggestions

--- app/src/main/java/travel/vola/android/ui/trip/viewmodel/TripViewModel.kt
+++ app/src/main/java/travel/vola/android/ui/trip/viewmodel/TripViewModel.kt
@@ -16,7 +16,6 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
