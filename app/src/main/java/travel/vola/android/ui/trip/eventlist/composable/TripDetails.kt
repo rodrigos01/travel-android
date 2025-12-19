405e0bb8e5bdd7b8971d2a36beab1adb0574f07b
Rodrigo Silva dos Santos <rodrigos01@meta.com>
2025-12-19 10:20:59 -0500

    Fixing bottom padding on trip details to allow for toolbar

--- app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/TripDetails.kt
+++ app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/TripDetails.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -29,6 +30,8 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -50,8 +53,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.PopupProperties
 import androidx.core.graphics.drawable.toBitmapOrNull
@@ -111,7 +116,7 @@ fun TripDetails(
     )
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 private fun TripDetails(
     state: TripViewModel.ViewState,
@@ -273,7 +278,11 @@ private fun TripDetails(
                 List(
                     state,
                     listScrollState,
-                    paddingValues,
+                    PaddingValues(
+                        top = paddingValues.calculateTopPadding(),
+                        start = paddingValues.calculateStartPadding(LocalLayoutDirection.current),
+                        bottom = paddingValues.calculateBottomPadding() + 24.dp + FloatingToolbarDefaults.ContainerSize
+                    ),
                     addPlanItemActionHandler,
                     onAddButonTapped,
                     onEmptyAddRowTapped,
