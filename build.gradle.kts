6489cbe2faa5994a5eaa113c1b54cfd8318a298a
Rodrigo <rodrigos.santos01@gmail.com>
2025-12-22 22:06:13 -0500

    Fixing kotlin versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.11.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("androidx.navigation.safeargs.kotlin") version "2.9.6" apply false
     id("com.google.gms.google-services") version "4.4.4" apply false
     kotlin("plugin.serialization") version "2.2.21"
