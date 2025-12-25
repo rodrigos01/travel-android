0fa83dda8da6403584e322132b583aeec9239ba7
Rodrigo <rodrigos.santos01@gmail.com>
2025-12-25 16:58:51 -0500

    Add flexible section list item

---
+++ app/src/main/java/travel/vola/android/ui/trip/eventlist/composable/AddFlexibleSectionListItem.kt
@@ -0,0 +1,86 @@
+package travel.vola.android.ui.trip.eventlist.composable
+
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import travel.vola.android.ui.theme.AppTheme
+import travel.vola.android.ui.trip.state.ManualAddLodgingItemState
+import travel.vola.android.ui.trip.state.ManualAddPlanState
+import travel.vola.android.ui.trip.state.ManualStartEndAddPlanState
+import java.time.ZonedDateTime
+
+@Composable
+fun AddFlexibleSectionListItem(
+    uiState: ManualStartEndAddPlanState,
+    onTextChanged: (CharSequence) -> Unit,
+    onUpdated: (
+        startDateTime: ZonedDateTime?,
+        startTimeSelected: Boolean,
+        endDateTime: ZonedDateTime?,
+        endTimeSelected: Boolean,
+    ) -> Unit,
+) {
+    StartEndAddPlanListItem(
+        uiState = uiState,
+        startTitle = { Text("Start") },
+        startTimeSelectorLabel = "Pick Start Time",
+        startLabelText = "Section Name",
+        startPlaceHolder = "Enter Section Name",
+        onStartTextChanged = onTextChanged,
+        endTitle = { Text("End") },
+        endTimeSelectorLabel = "Pick End Time",
+        showEndTimePickerButton = false,
+        endLabelText = "Pick End Time",
+        requiresEnd = false,
+        onUpdated = {
+                startDateTime,
+                startTimeSelected,
+                selectedStartSearchResultIndex,
+                endDateTime,
+                endTimeSelected,
+                _,
+            ->
+            onUpdated(
+                startDateTime,
+                startTimeSelected,
+                endDateTime,
+                endTimeSelected,
+            )
+        })
+}
+
+@PreviewLightDark
+@Composable
+fun AddFlexibleSectionListItemPreview() {
+    AppTheme {
+        Surface {
+            AddFlexibleSectionListItem(
+                uiState = ManualAddLodgingItemState(
+                    "", ZonedDateTime.now(),
+                    typeSelectionEnabled = false,
+                    startState = ManualAddPlanState(
+                        dateTime = null,
+                        minDateTime = ZonedDateTime.now(),
+                        isTimeSet = false,
+                        dateSelectionEnabled = false,
+                        locationText = null,
+                        searchResults = emptyList()
+                    ),
+                    endState = ManualAddPlanState(
+                        dateTime = null,
+                        minDateTime = ZonedDateTime.now(),
+                        isTimeSet = false,
+                        dateSelectionEnabled = true,
+                        locationText = null,
+                        searchResults = emptyList()
+                    ),
+                    deleteButtonEnabled = true,
+                    saveButtonEnabled = true,
+                ),
+                onTextChanged = {},
+                onUpdated = { _, _, _, _ -> },
+            )
+        }
+    }
+}
